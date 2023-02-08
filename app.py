60d31b578f7b8fd28e45188aaa79d9f4466c93ab
rick-1058138 <1058138@hr.nl>
2023-02-08 20:53:14 +0100

    FEATURE: added html day rows for rooster background

--- app.py
+++ app.py
@@ -34,6 +34,10 @@ def __init__(self, name):
 def hello_world():
     return render_template('index.html')
 
+@app.route("/rooster")
+def rooster():
+    return render_template('rooster.html')
+
 
 if __name__ == "__main__":
     app.run(host=FLASK_IP, port=FLASK_PORT, debug=FLASK_DEBUG)
