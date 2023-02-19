c97129ef2d2ea58fa11a176d517c15f02afb3499
Marinda-1058790 <113016517+Marinda-1058790@users.noreply.github.com>
2023-02-19 15:12:25 +0100

    UPDATE: Database still working

--- app.py
+++ app.py
@@ -22,58 +22,23 @@ app.app_context().push()
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
+class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False,
-                           default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    name = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"Student('{self.name}')"
 
 
-class Post(db.Model):
+class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False,
-                            default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False,
+                           default=datetime.utcnow())
+    end_date = db.Column(db.DateTime, nullable=False,
+                         default=datetime.utcnow())
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-
-
-# class Student(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(20), nullable=False)
-#     student_id = db.relationship('StudentForGroup', backref='idk', lazy=True)
-
-#     def __repr__(self):
-#         return f"Student('{self.name}')"
-
-
-# class StudentsForGroup:
-#     id = db.Column(db.Integer, primary_key=True)
-#     student_id = db.Column(db.Integer, db.ForeignKey(
-#         'student.id'), nullable=False)
-#     group_id = db.Column(db.Integer, db.ForeignKey(
-#         'group.id'), nullable=False)
-
-
-# class Group(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     start_date = db.Column(db.DateTime, nullable=False,
-#                            default=datetime.cestnow())
-#     end_date = db.Column(db.DateTime, nullable=False,
-#                          defaulr=datetime.cestnow())
-#     group_id = db.relationship('StudentForGroup', backref='idk', lazy=True)
-
-#     def __repr__(self):
-#         return f"Group('{self.start_date}', '{self.end_date}')"
+        return f"Group('{self.start_date}', '{self.end_date}')"
 
 
 @app.route("/")
