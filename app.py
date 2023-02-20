e0de6074575aa1555768ca836902250d215bb0f0
Celeste-1064634 <1064634@hr.nl>
2023-02-20 11:33:59 +0100

    ADDED: routes welcome/overview-page

--- app.py
+++ app.py
@@ -57,9 +57,20 @@ def login():
 def code_input():
     return render_template("code-input.html")
 
+
 @app.route("/les_overzicht")
 def les_overzicht():
     return render_template("les_overzicht.html")
+
+
+@app.route("/overview_page")
+def overview_page():
+    return render_template('overview_page.html')
+
+
+@app.route("/welcome_page")
+def welcome_page():
+    return render_template('welcome_page.html')
 
 
 @app.route("/test")
